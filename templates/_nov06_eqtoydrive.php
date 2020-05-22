8d3e9713fecd4407a883fa3c820b63bd527e7da9
MiApple <aliceding@miapple.ca>
2020-05-21 20:45:38 -0400

    Add files via upload

--- templates/_nov06_eqtoydrive.php
+++ templates/_nov06_eqtoydrive.php
@@ -62,8 +62,7 @@
 	</div>
 	<div class="row">
 		<div class="col">
-			<a href="http://www.makeawisheo.ca/">
-			<img src="https://www.eqhomes.ca/wp-content/uploads/2018/eqhomestoydrive/images/eqtoydrive_13.jpg"   class="img-fluid" alt=""></div></a>
+			<img src="https://www.eqhomes.ca/wp-content/uploads/2018/eqhomestoydrive/images/eqtoydrive_13.jpg"   class="img-fluid" alt=""></div>
 	</div>
 	<div class="row">
 		<div class="col">
@@ -81,19 +80,14 @@
 <table border="0" width="90%" cellspacing="0" cellpadding="0" align="center">
 <tbody>
 <tr>
-<td><a href="https://www.google.ca/maps/place/78+Equinelle+Dr,+Kemptville,+ON+K0G+1J0/data=!4m2!3m1!1s0x4ccdea57a4c24c65:0xd5db8a16e859f304?sa=X&amp;ved=0ahUKEwjyo_K2j9XXAhWB24MKHWoIDKYQ8gEIJzAA" target="_blank" rel="noopener"><img class="size-full wp-image-5236 alignright" src="https://www.eqhomes.ca/wp-content/uploads/2017/11/equinelle-map.png" alt="" width="195" height="197" /></a>
-<strong>eQuinelle</strong> - Kemptville<br>
+<td><a href="https://www.google.ca/maps/place/78+Equinelle+Dr,+Kemptville,+ON+K0G+1J0/data=!4m2!3m1!1s0x4ccdea57a4c24c65:0xd5db8a16e859f304?sa=X&amp;ved=0ahUKEwjyo_K2j9XXAhWB24MKHWoIDKYQ8gEIJzAA" target="_blank" rel="noopener"><img class="size-full wp-image-5236 alignright" src="https://www.eqhomes.ca/wp-content/uploads/2017/11/equinelle-map.png" alt="" width="195" height="197" /></a><strong>
+eQuinelle</strong> - Kemptville<br>
 Bungalows, Towns &amp; Legends<br>
 <a href="https://www.google.ca/maps/place/78+Equinelle+Dr,+Kemptville,+ON+K0G+1J0/data=!4m2!3m1!1s0x4ccdea57a4c24c65:0xd5db8a16e859f304?sa=X&amp;ved=0ahUKEwjyo_K2j9XXAhWB24MKHWoIDKYQ8gEIJzAA" target="_blank" rel="noopener">78 Equinelle Dr., Kemptville, ON K0G 1J0<br>
 </a>
-Saturday and Sunday: 12pm - 5pm
-<br>
-(Closed on Holidays)
-<br>Tel: 613-258-6488
+Tel: 613-258-6488
 Email: <a href="mailto:sales@equinelle.ca">sales@equinelle.ca</a><br>
-Detached Models: Sawgrass, Alexander, Hamilton
-<br><br><br>
-</td>
+Detached Models: Sawgrass, Alexander, Hamilton</td>
 <td></td>
 </tr>
 <tr>
@@ -101,30 +95,19 @@ Detached Models: Sawgrass, Alexander, Hamilton
 Bungalows, Two-Storey, Towns &amp; Condos<br>
 <a href="https://www.google.ca/maps/place/Fernbank+Crossing+-+eQ+Homes/@45.2696097,-75.8904708,17z/data=!3m1!4b1!4m5!3m4!1s0x4ccdff2355004bb5:0xb6f7593fa9e25976!8m2!3d45.2696097!4d-75.8882768" target="_blank" rel="noopener">632 Cope Drive,<br></a>
 <a href="https://www.google.ca/maps/place/Fernbank+Crossing+-+eQ+Homes/@45.2696097,-75.8904708,17z/data=!3m1!4b1!4m5!3m4!1s0x4ccdff2355004bb5:0xb6f7593fa9e25976!8m2!3d45.2696097!4d-75.8882768" target="_blank" rel="noopener">Stittsville, ON K4K 1T2</a><br>
-Monday to Thursday 12pm - 7pm<br>
-Friday: 11am - 5pm<br>
-Saturday and Sunday: 12pm - 5pm<br>
-(Closed on Holidays)<br>
 Tel: 613-404-8075<br>
 Email: <a href="mailto:sales@fernbankcrossing.com">sales@fernbankcrossing.com</a><br>
 Detached Models: Billings, Augusta, Blackburn, Anderson<br>
-Condo Models: Alder, Rosewood, Jatoba, Cypress
-<br><br><br>
-</td>
+Condo Models: Alder, Rosewood, Jatoba, Cypress</td>
 <td></td>
 </tr>
 <tr>
 <td><a href="https://www.google.ca/maps/place/Greystone+Village+Presentation+Centre+-+eQ+Homes/@45.4091783,-75.6799615,17z/data=!4m15!1m9!2m8!1sHotels!3m6!1sHotels!2s45.4091783,+-75.6777728!3s0x4cce05bd7c85256d:0xde3693b341b4e8ca!4m2!1d-75.6777728!2d45.4091783!3m4!1s0x0:0x6e1171a55d34dcdb!8m2!3d45.4096822!4d-75.6776778" target="_blank" rel="noopener"><img class="size-full wp-image-5238 alignright" src="https://www.eqhomes.ca/wp-content/uploads/2017/11/greystone-map.png" alt="" width="192" height="193" /></a><strong>Greystone Village</strong> - Ottawa<br>
 Two-Storey, Towns, Condos<br>
 <a href="https://www.google.ca/maps/place/Greystone+Village+Presentation+Centre+-+eQ+Homes/@45.4091783,-75.6799615,17z/data=!4m15!1m9!2m8!1sHotels!3m6!1sHotels!2s45.4091783,+-75.6777728!3s0x4cce05bd7c85256d:0xde3693b341b4e8ca!4m2!1d-75.6777728!2d45.4091783!3m4!1s0x0:0x6e1171a55d34dcdb!8m2!3d45.4096822!4d-75.6776778" target="_blank" rel="noopener">175A Main Street, Ottawa, ON K1S 1C3</a><br>
-Monday to Thursday 12pm - 7pm<br>
-Friday: 11am - 5pm<br>
-Saturday and Sunday: 12pm - 5pm<br>
-(Closed on Holidays)<br>
 Tel: 613-569-3043<br>
 Email: <a href="mailto:sales@greystonevillage.ca">sales@greystonevillage.ca</a><br>
-Condo Models: Model 14
-<br><br><br></td>
+Condo Models: Model 14</td>
 <td></td>
 </tr>
 <tr>
@@ -132,44 +115,18 @@ Condo Models: Model 14
 Bungalows, Two-Storey, Towns &amp; Condos<br>
 <a href="https://www.google.ca/maps/place/Clarence+Crossing+-+eQ+Homes/@45.5569577,-75.26779,15z/data=!4m2!3m1!1s0x0:0xf46a8b51d4a7cbf1?sa=X&amp;ved=0ahUKEwio5OXekdXXAhUEyYMKHQiyA3MQ_BIIfzAK" target="_blank" rel="noopener">235 L’etang Street,</a><br>
 <a href="https://www.google.ca/maps/place/Clarence+Crossing+-+eQ+Homes/@45.5569577,-75.26779,15z/data=!4m2!3m1!1s0x0:0xf46a8b51d4a7cbf1?sa=X&amp;ved=0ahUKEwio5OXekdXXAhUEyYMKHQiyA3MQ_BIIfzAK" target="_blank" rel="noopener">Clarence-Rockland, ON K4K 1T2</a><br>
-Monday to Thursday 12pm - 7pm<br>
-Friday: 11am - 5pm<br>
-Saturday and Sunday: 12pm - 5pm<br>
-(Closed on Holidays)<br>
 Tel: 613-862-0167<br>
 Email: <a href="mailto:sales@clarencecrossing.com">sales@clarencecrossing.com</a><br>
 Detached Models: Dumont, Alexander<br>
-Attached Model: Augusta
-<br><br><br></td>
+Attached Model: Augusta</td>
 <td></td>
 </tr>
 <tr>
 <td><a href="https://www.google.ca/maps/place/Pathways+at+Findlay+Creek+-+eQ+Homes/@45.3100288,-75.5904987,17z/data=!4m13!1m7!3m6!1s0x4cce0a175efc1981:0x120ab985441d5ccc!2s4828+Bank+St,+Gloucester,+ON+K1X+1G6!3b1!8m2!3d45.3100288!4d-75.58831!3m4!1s0x4cce0a220551c477:0x99c3d50a605fa1ef!8m2!3d45.310029!4d-75.5883101" target="_blank" rel="noopener"><img class="size-full wp-image-5240 alignright" src="https://www.eqhomes.ca/wp-content/uploads/2017/11/pathways-map.png" alt="" width="192" height="194" /></a><strong>Pathways at Findlay Creek</strong> - Letrim<br>
 Bungalows, Two-Storey &amp; Towns<br>
 <a href="https://www.google.ca/maps/place/Pathways+at+Findlay+Creek+-+eQ+Homes/@45.3100288,-75.5904987,17z/data=!4m13!1m7!3m6!1s0x4cce0a175efc1981:0x120ab985441d5ccc!2s4828+Bank+St,+Gloucester,+ON+K1X+1G6!3b1!8m2!3d45.3100288!4d-75.58831!3m4!1s0x4cce0a220551c477:0x99c3d50a605fa1ef!8m2!3d45.310029!4d-75.5883101" target="_blank" rel="noopener">4828 Bank St. Ottawa, ON K1X 1G6</a><br>
-Monday to Thursday 12pm - 7pm<br>
-Friday 11am - 5pm<br>
-Saturday and Sunday 12pm - 5pm<br>
-(Closed on Holidays)<br>
 Tel: 613.822.7247<br>
-Email: <a href="mailto:sales@eqpathways.ca">sales@eqpathways.ca</a>
-<br><br><br></td>
-<td></td>
-</tr>
-<tr>
-<td><img class="wp-image-5849 size-full alignright" src="https://www.eqhomes.ca/wp-content/uploads/2018/11/riverwalk-map.png" alt="" width="192" height="201" />
-<strong>Riverwalk Sales Centre</strong><br>
-<a href="https://goo.gl/maps/gVPNsrr7vh52">78 Equinelle Drive in Kemptville, <br>
-off County Road 44</a><br>
-Monday to Thursday 12pm - 7pm<br>
-Friday 11am - 5pm<br>
-Saturday and Sunday 12pm - 5pm<br>
-(Closed on Holidays)<br>
-
-<strong>Tel: </strong>613-618-3204<br>
-<strong>Email: </strong><a href="mailto:CWarnerJacobs@eqhomes.ca">CWarnerJacobs@eqhomes.ca</a>
-<br><br><br>
-</td>
+Email: <a href="mailto:sales@eqpathways.ca">sales@eqpathways.ca</a></td>
 <td></td>
 </tr>
 </tbody>
