20824c9a7120999ed0b0e26a8cf3d6f1acc0c999
MiApple <aliceding@miapple.ca>
2020-05-21 20:42:33 -0400

    Add files via upload

--- inc/customizer.php
+++ inc/customizer.php
@@ -1,1 +1,26 @@
- 
+<?php
+/**
+ * EQ HOMES Theme Customizer
+ *
+ * @package EQ HOMES
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function eq_homes_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'eq_homes_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function eq_homes_customize_preview_js() {
+	wp_enqueue_script( 'eq_homes_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
+}
+add_action( 'customize_preview_init', 'eq_homes_customize_preview_js' );
